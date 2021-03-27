ea4ee9d5ee2cfd1d591c5d2ba83eee505919fdc4
LikhithaKV <reachlikhitha@gmail.com>
2021-03-27 19:24:58 +0530

    commiting through eclipse

--- src/test/java/com/hms/doctortest/CreatePatientTest.java
+++ src/test/java/com/hms/doctortest/CreatePatientTest.java
@@ -1,5 +1,10 @@
 package com.hms.doctortest;
 
-public class CreatePatientTest {
+import org.testng.annotations.Test;
 
+public class CreatePatientTest {
+@Test
+public void cp() {
+	System.out.println("patient created");
+}
 }
