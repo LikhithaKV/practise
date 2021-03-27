cbaf300909951c0d9b0b7e50723fa3f67330cd7d
LikhithaKV <reachlikhitha@gmail.com>
2021-03-27 19:29:46 +0530

    commiting form gitbash

--- src/test/java/com/hms/doctortest/ProvideAppointmentToPatientTest.java
+++ src/test/java/com/hms/doctortest/ProvideAppointmentToPatientTest.java
@@ -1,5 +1,11 @@
 package com.hms.doctortest;
 
+import org.testng.annotations.Test;
+
 public class ProvideAppointmentToPatientTest {
 
+	@Test
+	public void pa() {
+		System.out.println("appointment created");
+	}
 }
